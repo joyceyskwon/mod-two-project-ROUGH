eab1872d1206ac67c092486f6f20b0200b25ddff
github username <github email address>
2018-12-27 23:04:34 -0500

    added login page with sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :reservations
   resources :favorites
   resources :users
-  
-  get '/users/new', to: 'users#new'
   resources :relationships
   resources :restaurants, only: [:index, :show]
   resources :companies, only: [:index, :show]
